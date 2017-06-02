b975cd4946025a89dab5593a7eb4349d6974b2ba
Yesenia <yeseniasescarcega@gmail.com>
2017-06-02 16:56:46 -0500

    Se guardan datos de formulario mediante jQuery.

--- agendaContactos/assets/js/app.js
+++ agendaContactos/assets/js/app.js
@@ -1,5 +1,27 @@
 console.log("Bienvenido");
-$(document).ready(function(){
+var contador = 0;
+// Funciones
+var modal = function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-  });
+};
+
+var datosForm = function () {
+  contador++;
+  var $nombre = $("#icon_prefix").val();
+  var $tel = $("#icon_telephone").val();
+  var $cumple = $("#date_range").val();
+
+  console.log($nombre);
+  console.log($tel);
+  console.log($cumple);
+  mostrarDatos($nombre, $tel, $cumple);
+};
+
+function mostrarDatos(nombre, tel, cumple) {
+
+}
+
+// Eventos
+$("#enviar").click(datosForm);
+$(document).ready(modal);
